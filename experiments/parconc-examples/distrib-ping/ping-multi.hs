fec36cc2bb361d5c66a4d6cc88b82dc360f65308
Justin Dawson <jdawson@ku.edu>
2014-03-27 11:44:37 -0500

    Added fib message and handler to multi machine ping

--- experiments/parconc-examples/distrib-ping/ping-multi.hs
+++ experiments/parconc-examples/distrib-ping/ping-multi.hs
@@ -14,6 +14,7 @@ import DistribUtils
 -- <<Message
 data Message = Ping ProcessId
              | Pong ProcessId
+             | Fib (ProcessId, Int)
   deriving (Typeable, Generic)          -- <1>
 
 instance Binary Message                 -- <2>
@@ -27,9 +28,29 @@ pingServer = do
   mypid <- getSelfPid                              -- <3>
   send from (Pong mypid)                           -- <4>
 -- >>
+fib:: Int->Int
+fib n  
+   |n < 2 = 1
+   | otherwise = (fib (n-2)) + (fib (n-1)) + 1
+
+-- <<fibServer
+fibServer:: Process()
+fibServer = do
+  mypid <- getSelfPid
+  m <- expect
+  case m of
+    Fib (from, param)-> do say $ printf "Fib received from %s" (show from) 
+                           send from (Fib (mypid,(fib param)))
+    Ping from -> do say $ printf "ping received from %s" (show from) -- <2>
+                    send from (Pong mypid)                           -- <4>
+    Pong from-> do say $ "Error Pong Message received"
+                   send from (Pong mypid) 
+
+-- >>
 
 -- <<remotable
-remotable ['pingServer]
+--remotable ['pingServer]
+remotable ['fibServer]
 -- >>
 
 -- <<master
@@ -38,16 +59,27 @@ master peers = do
 
   ps <- forM peers $ \nid -> do                      -- <2>
           say $ printf "spawning on %s" (show nid)
-          spawn nid $(mkStaticClosure 'pingServer)
+          spawn nid $(mkStaticClosure 'fibServer)
 
   mypid <- getSelfPid
 
   forM_ ps $ \pid -> do                              -- <3>
     say $ printf "pinging %s" (show pid)
     send pid (Ping mypid)
+    send pid (Ping mypid)
 
   waitForPongs ps                                    -- <4>
 
+  forM_ ps $ \pid -> do                              -- <3>
+    say $ printf "pinging2 %s" (show pid)
+    send pid (Ping mypid)
+  waitForPongs ps
+  forM_ ps $ \pid -> do                              -- <3>
+    say $ printf "sending fib 10 to  %s" (show pid)
+    send pid (Fib (mypid,10))
+
+  waitForPongs ps
+ 
   say "All pongs successfully received"
   terminate
 
@@ -57,6 +89,8 @@ waitForPongs ps = do
   m <- expect
   case m of
     Pong p -> waitForPongs (filter (/= p) ps)
+    Fib (proc, result)-> do say ("Fib Result is: " ++ (show result) )
+                            waitForPongs (filter (/= proc) ps)
     _  -> say "MASTER received ping" >> terminate
 -- >>
 
